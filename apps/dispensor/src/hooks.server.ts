651c1f9b91ff74392e6b91c9164c9c8271175754
ihsanvp <ihsanvp2738@gmail.com>
2024-04-04 17:50:08 +0530

    test(dispensor): env vars

--- apps/dispensor/src/hooks.server.ts
+++ apps/dispensor/src/hooks.server.ts
@@ -1,29 +1,33 @@
+import { FIREBASE_ADMIN_KEY } from '$env/static/private';
 import { AuthConfig } from '$lib/auth/config.auth';
 import { getSession } from '$lib/auth/session.auth';
 import { isApiRequest, isProtectedRoute } from '$lib/auth/utils.auth';
 import { initializeDatabase } from '$lib/utils/db';
 import { getUserRepository } from '@e-ration/database';
-import { redirect } from '@sveltejs/kit';
+import { json, redirect } from '@sveltejs/kit';
 
 /** @type {import("@sveltejs/kit").Handle} */
 export async function handle({ event, resolve }) {
-	initializeDatabase();
+	// initializeDatabase();
 
-	if (isProtectedRoute(event, AuthConfig.excludeRoutes)) {
-		const session = getSession(event, AuthConfig);
-		if (!session) {
-			throw redirect(307, '/login');
-		}
-		event.locals.session = session;
-		const user = await getUserRepository().findById(session.uid);
-		if (user) {
-			event.locals.user = user.toJson();
-		}
-		if (!user || user.category == null) {
-			if (event.url.pathname != '/register' && event.url.pathname != '/api/auth/register') {
-				throw redirect(307, '/register');
-			}
-		}
-	}
-	return resolve(event);
+	// if (isProtectedRoute(event, AuthConfig.excludeRoutes)) {
+	// 	const session = getSession(event, AuthConfig);
+	// 	if (!session) {
+	// 		throw redirect(307, '/login');
+	// 	}
+	// 	event.locals.session = session;
+	// 	const user = await getUserRepository().findById(session.uid);
+	// 	if (user) {
+	// 		event.locals.user = user.toJson();
+	// 	}
+	// 	if (!user || user.category == null) {
+	// 		if (event.url.pathname != '/register' && event.url.pathname != '/api/auth/register') {
+	// 			throw redirect(307, '/register');
+	// 		}
+	// 	}
+	// }
+	// return resolve(event);
+	return json({
+		key: FIREBASE_ADMIN_KEY
+	});
 }
