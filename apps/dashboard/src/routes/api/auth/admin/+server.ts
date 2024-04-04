a2ec31c970d8e50fdf60729ac041c09d55162e5d
ihsanvp <ihsanvp2738@gmail.com>
2024-04-04 19:49:04 +0530

    fix(dashboard): set admin api route security

--- apps/dashboard/src/routes/api/auth/admin/+server.ts
+++ apps/dashboard/src/routes/api/auth/admin/+server.ts
@@ -1,5 +1,7 @@
+import { AuthConfig } from '$lib/auth/config.auth.js';
+import { getSession } from '$lib/auth/session.auth.js';
 import { getFirebaseAdminAuth } from '$lib/firebase/firebase.admin.js';
-import { text } from '@sveltejs/kit';
+import { error, text } from '@sveltejs/kit';
 import { z } from 'zod';
 
 const schema = z.object({
@@ -7,10 +9,21 @@ const schema = z.object({
 });
 
 /** @type {import("./$types").RequestHandler} */
-export async function POST({ request }) {
-	const body = await request.json();
+export async function POST(event) {
+	const body = await event.request.json();
 	const data = schema.parse(body);
+	const session = getSession(event, AuthConfig);
+	if (!session) {
+		return error(401);
+	}
 	const auth = getFirebaseAdminAuth();
+	const user = await auth.getUser(data.uid);
+	if (!user) {
+		return error(401);
+	}
+	if (!user.customClaims || !user.customClaims['admin']) {
+		return error(401);
+	}
 	await auth.setCustomUserClaims(data.uid, {
 		admin: true
 	});
